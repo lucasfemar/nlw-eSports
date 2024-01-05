d729a8105ac77952206255d34cc8e01bd638814b
Lucas Marcelo <lucasdeveloper98l@gmail.com>
2024-01-05 00:09:39 -0300

    `Aula 4` part 1

--- mobile/src/screens/Home/index.tsx
+++ mobile/src/screens/Home/index.tsx
@@ -3,16 +3,24 @@ import logoImg from '../../assets/logo-nlw-esports.png';
 import { styles } from './styles';
 import { Heading } from '../../components/Heading';
 import { GAMES } from '../../utils/games';
-import { GameCard } from '../../components/GameCard';
+import { GameCard, GameCardProps } from '../../components/GameCard';
+import { useEffect, useState } from 'react';
 
 export function Home() {
+    const [games, setGames] = useState<GameCardProps[]>([]);
+
+    useEffect(() => {
+        fetch('http://172.27.202.6:3333/games')
+            .then((response) => response.json())
+            .then((data) => setGames(data));
+    }, []);
     return (
         <View style={styles.container}>
             <Image source={logoImg} style={styles.logo} />
             <Heading title="Encontre seu duo!" subtitle="Selecione o game que deseja jogar..." />
             <FlatList
                 contentContainerStyle={styles.contentList}
-                data={GAMES}
+                data={games}
                 keyExtractor={(item) => item.id}
                 renderItem={({ item }) => <GameCard data={item} />}
                 showsHorizontalScrollIndicator={false}
