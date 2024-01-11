2c4d1329543e4edd3137d446b483b75fb7764d80
lucasfemar <lucasdeveloper98l@gmail.com>
2024-01-10 21:30:12 -0300

    `Aula 4` part 2

--- mobile/src/screens/Home/index.tsx
+++ mobile/src/screens/Home/index.tsx
@@ -1,21 +1,21 @@
-import { Image, View, FlatList } from 'react-native';
+import { useEffect, useState } from 'react';
+import { Image, FlatList } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
 import logoImg from '../../assets/logo-nlw-esports.png';
 import { styles } from './styles';
 import { Heading } from '../../components/Heading';
-import { GAMES } from '../../utils/games';
 import { GameCard, GameCardProps } from '../../components/GameCard';
-import { useEffect, useState } from 'react';
 
 export function Home() {
     const [games, setGames] = useState<GameCardProps[]>([]);
 
     useEffect(() => {
-        fetch('http://172.27.202.6:3333/games')
+        fetch('http://192.168.15.112:3333/games')
             .then((response) => response.json())
             .then((data) => setGames(data));
     }, []);
     return (
-        <View style={styles.container}>
+        <SafeAreaView style={styles.container}>
             <Image source={logoImg} style={styles.logo} />
             <Heading title="Encontre seu duo!" subtitle="Selecione o game que deseja jogar..." />
             <FlatList
@@ -26,6 +26,6 @@ export function Home() {
                 showsHorizontalScrollIndicator={false}
                 horizontal
             />
-        </View>
+        </SafeAreaView>
     );
 }
